07c27069f555c18a0211162c07543c008eec4e8c
shantelletoh <shantelleschool@gmail.com>
2022-06-17 12:46:15 -0700

    connect to mongodb database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,12 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.gpyaw.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})  // connect to database
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false); // auto-handled for Mongoose v6 and above
