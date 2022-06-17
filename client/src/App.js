9a3726b7edf965bdc309dec187f33b745ad9caf8
shantelletoh <shantelleschool@gmail.com>
2022-06-17 14:38:22 -0700

    styling client

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
 import memories from './images/memories.png';
+import useStyles from './styles';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
