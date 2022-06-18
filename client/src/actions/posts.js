d57bef05e66ddca9bec527d8c872cf63eae79a60
shantelletoh <shantelleschool@gmail.com>
2022-06-18 16:21:41 -0700

    implement updatePost

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -17,6 +17,16 @@ export const createPost = (post) => async (dispatch) => {
 
         dispatch({type: 'CREATE', payload: data});
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const {data} = await api.updatePost(id, post);
+
+        dispatch({type: 'UPDATE', payload: data});
+    } catch (error) {
+        console.log(error.message);
     }
 }
