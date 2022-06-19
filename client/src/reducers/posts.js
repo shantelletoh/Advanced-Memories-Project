58438f41d075941def2f75f9faac99a556faf53e
shantelletoh <shantelleschool@gmail.com>
2022-06-18 18:46:24 -0700

    implement likeCount

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,7 +3,8 @@ export default (posts = [], action) => {
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload); // return all posts but filter the deleted post
         case 'UPDATE':
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+        case 'LIKE': // this allows both UPDATE and LIKE cases to use the same return statement
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post); // search for post that changed and returned the changed post, else return original post
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
