195ace54d29f698017597ab22e83d831b7575aea
shantelletoh <shantelleschool@gmail.com>
2022-06-18 19:03:00 -0700

    clean up code: change actionTypes to constants

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,13 +1,14 @@
+import {FETCH_ALL, CREATE, UPDATE, DELETE} from '../constants/actionTypes';
+
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload); // return all posts but filter the deleted post
-        case 'UPDATE':
-        case 'LIKE': // this allows both UPDATE and LIKE cases to use the same return statement
+        case UPDATE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post); // search for post that changed and returned the changed post, else return original post
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload]; // array of posts, add a new post stored in action.payload
         default:
             return posts;
