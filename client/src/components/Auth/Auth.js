4a76d05aa1347191ba83319274a17e0779474a4f
shantelletoh <shantelleschool@gmail.com>
2022-06-21 11:38:21 -0700

    feat(auth): create sign-in form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Avatar, Button, Container, Grid, Paper, Typography} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles from './styles';
+import Input from './Input';
 
 const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false);
+    
+    const handleShowPassword = () => setShowPassword((prevShowPassword) =>!prevShowPassword) // previous state: whenever we're changing state using the old state, have to create a callback function
+    
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
 
     return (
-        <div>Auth</div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">Sign In</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                    </Grid>
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        Sign In
+                    </Button>
+                </form>
+            </Paper>
+        </Container>
     );
 };
 
