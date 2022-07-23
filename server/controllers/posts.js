5e76bece200c11bc0b5e59909f6e05e4e28757e1
shantelletoh <shantelleschool@gmail.com>
2022-07-23 12:55:38 -0700

    feat(likes): add dislike toggle, limit liking once per user

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,11 @@
 // handlers for routes
 
+import express from 'express';
 import mongoose from 'mongoose';
 
 import PostMessage from '../models/postMessage.js';
+
+const router = express.Router();
 
 export const getPosts = async (req, res) => {
     try {
@@ -51,12 +54,24 @@ export const deletePost = async (req, res) => {
 }
 
 export const likePost = async (req, res) => {
+
+    if (!req.userId) return res.json({message: "Unauthenticated"});
     const {id} = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, {new: true});
+    const index = post.likes.findIndex((id) => id === String(req.userId)) // check if user id already liked the post (is already in the like section)
+
+    if (index === -1) { // like the post
+        post.likes.push(req.userId);
+    }
+    else { // dislike the post
+        post.likes = post.likes.filter((id) => id !== String(req.UserId)); // array of all the likes except for current user
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {new: true}); // update entire post
 
     res.json(updatedPost);
 }
+
+export default router;
